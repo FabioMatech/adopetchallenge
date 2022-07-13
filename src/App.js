764cc6bbfa884f87ebd253d94ab52fb1c1570933
Fabio <clubeteczagem@gmail.com>
2022-07-13 19:05:44 -0300

    Atualizando o readme

--- src/App.js
+++ src/App.js
@@ -2,18 +2,7 @@ import { Cabecalho } from './componentes/pagina/Cabecalho';
 import { Conteudo } from './rotas/Route';
 import {Fragment} from 'react';
 import { Rodape } from './componentes/pagina/Rodape';
-import { Globalstyle } from './estilos/GlobalStyle';
-import styled from 'styled-components';
-
-const Pagina = styled.body`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-`
-
-const BoxConteudo = styled.main`
-  flex-grow: 1;
-`
+import { Globalstyle, Pagina, BoxConteudo } from './estilos/GlobalStyle';
 
 
 export const App = () => (
@@ -29,7 +18,7 @@ export const App = () => (
         </BoxConteudo>
 
         <Rodape /> 
-          
+
     </Fragment>
  </Pagina>
  
--- src/estilos/GlobalStyle.js
+++ src/estilos/GlobalStyle.js
@@ -1,5 +1,15 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 export const Globalstyle = createGlobalStyle`
    
+`
+export const Pagina = styled.body`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  
+`
+
+export const BoxConteudo = styled.main`
+  flex-grow: 1;
 `
