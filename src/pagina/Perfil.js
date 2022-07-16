9813e79fa009adf4fa42ae4d1e4d0c679bdae3b3
Fabio <clubeteczagem@gmail.com>
2022-07-16 16:35:03 -0300

    botao formulario

--- src/pagina/Perfil.js
+++ src/pagina/Perfil.js
@@ -1,7 +1,38 @@
+import styled from 'styled-components'
 import Cachorro from "../imagens/cachorro.jpg"
 import { FormularioPerfil, WrapperPerfil,  
         InputPerfil, TituloFormulario, LabelPerfil, InputImg, TextoPerfil,
-        WrapperBotao, BotaoPerfil} from "../componentes/elementos/FormularioPerfil";
+        } from "../componentes/elementos/FormularioPerfil";
+
+const CreditoImagem = styled.p`
+    text-align: right;
+    font-size: 0.7em;
+`
+const CliqueFoto = styled.p`
+    color: red;
+    font-size: 0.8em;
+    text-align: center;
+    padding-top: 0.5em;
+`
+const WrapperBotao = styled.div`
+    display: flex;
+    flex-direction: row;
+    text-align: center;
+    padding: 2em;
+`
+
+
+const BotaoFormulario = styled.button`
+     border-radius: 3px;
+     padding: 0.5rem 0;
+     margin: 0.5rem 1rem;
+     width: 11rem;
+     background: #FC7071 ;
+     color: white;
+     border: 2px solid #3772FF;
+     margin: auto;
+     
+`
 
 export const Perfil = () => (
     <WrapperPerfil>
@@ -16,9 +47,9 @@ export const Perfil = () => (
         
 
             <LabelPerfil for="foto">Foto </LabelPerfil>        
-            <InputImg type="image" id="image" alt="Login" src={Cachorro} />
-        
-             
+            <InputImg type="image" id="image" alt="Foto Perfil" src={Cachorro} />
+            <CreditoImagem>Imagem de <a href="https://pixabay.com/pt/users/pixel2013-2364555/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3380548">S. Hermann &amp;amp; F. Richter</a> por <a href="https://pixabay.com/pt/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3380548">Pixabay</a></CreditoImagem>
+            <CliqueFoto>Clique na foto para editar</CliqueFoto>
         
         <LabelPerfil for="nome">Nome </LabelPerfil>
         <InputPerfil type="text" id="nomeperfil" name="nome" placeholder="Aqui será seu nome" />
@@ -28,16 +59,15 @@ export const Perfil = () => (
         
         <LabelPerfil for="cidade">Cidade </LabelPerfil>
         <InputPerfil type="text" id="cidade" name="cidade" placeholder="São Paulo" />
-       
-        
-        
+
+      
+        <WrapperBotao>
+         <BotaoFormulario type="submit" value="Click Me" >Salvar</BotaoFormulario>
+         </WrapperBotao>
         
         </FormularioPerfil>   
-       <WrapperBotao>
-        <BotaoPerfil href="/Home">
-           Salvar   
-        </BotaoPerfil>
-        </WrapperBotao>
+       
+      
        
        
      
